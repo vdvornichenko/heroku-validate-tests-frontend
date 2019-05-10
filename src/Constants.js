e8027e8565e2a5ef885896a1d28d57709015b016
Denis Boreyko <dee-ded@mail.ru>
2019-05-10 16:02:17 +0300

    Corrected

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 //Storage for const variables that can be changed
-export var HTTP_USER_CREDS_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/getUsers' : '';
-export var HTTP_USERS_INFO_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/usersInfo' : '';
-export var HTTP_FILE_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/userFile' : '';
+export var HTTP_USER_CREDS_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/getUsers' : 'https://task-validation-lc.herokuapp.com/getUsers';
+export var HTTP_USERS_INFO_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/usersInfo' : 'https://task-validation-lc.herokuapp.com/usersInfo';
+export var HTTP_FILE_URL = (window.location.href.includes('localhost')) ? 'http://localhost:8080/userFile' : 'https://task-validation-lc.herokuapp.com/userFile';
 
 export const NOT_FOUND_MESSAGE = 'Не найден файл';
 export const NO_SELECTED_USERS_MESSAGE = 'You must select at least 1 user';
